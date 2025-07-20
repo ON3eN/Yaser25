15a52d78991828ef606359c4ceca1f9f8f370456
ON3eN <goto6946@gmail.com>
2025-07-20 21:53:26 +0300

    المنتجات فالرئيسية

--- Yaser25/urls.py
+++ Yaser25/urls.py
@@ -2,20 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import render
-
-# عرض الصفحة الرئيسية من مجلد templates
-def home(request):
-    return render(request, 'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),  # الصفحة الرئيسية
-    path('', include('store.urls')),
+    path('', include('store.urls')),       # الصفحة الرئيسية عبر store/views.py
     path('order/', include('order.urls')),
     path('account/', include('account.urls')),
 ]
 
-# دعم تحميل الصور عند تفعيل debug
+# دعم تحميل الصور أثناء التطوير فقط
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
